b16c072a045a278f46be66135e0b7c094e3282f1
Vinayak Bagi <vinayakbagi@gmail.com>
2020-09-27 21:49:06 +0530

    HOme page content changed and removed sub section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   homepageContent = [
     { title: 'Supply Chain',
-      description: `If you go to a Supermarket and pick up a few items off the shelf from electronics and white goods or even clothes and look at the labels, the chances are that you will find them having been manufactured in China or Mexico. The coffee pods you buy to use for your everyday use comes from Africa. Computers have been shipped out of South American Factories and Soft furnishings on the shelves are from India and Hong Kong.
-        Global markets are expanding beyond borders and re-defining the way demand and supplies are managed. Global companies are driven by markets across continents. To keep the cost of manufacturing down, they are forced to keep looking to set up production centers where the cost of raw materials and labor is cheap. Sourcing of raw materials and vendors to supply the right quality, quantity and at right price calls for dynamic procurement strategy spanning across countries.
-        With the above scenario you find companies procuring materials globally from various vendors to supply raw materials to their factories situated in different continents. The finished goods out of these different factory locations then pass through various chains of distribution network involving warehouses, exports to different countries or local markets, distributors, retailers and finally to the end customer.
-        In simple language, managing all of the above activities in tandem to manage demand and supply on a global scale is Supply Chain Management. As per definition SCM is the management of a network of all business processes and activities involving procurement of raw materials, manufacturing and distribution management of Finished Goods. SCM is also called the art of management of providing the Right Product, At the Right Time, Right Place and at the Right Cost to the Customer.`,
+      description: `Global companies are driven by markets across continents. To keep the cost of manufacturing down,
+      they are forced to keep looking to set up production centers where the cost of raw materials and labor is cheap.
+      Sourcing of raw materials and vendors to supply the right quality, quantity and at right price calls for dynamic
+      procurement strategy spanning across countries. With the above scenario you find companies procuring materials
+      globally from various vendors to supply raw materials to their factories situated in different continents. The
+      finished goods out of these different factory locations then pass through various chains of distribution network
+      involving warehouses, exports to different countries or local markets, distributors, retailers and finally to
+      the end customer.`,
       mainUrl: './assets/supply-chain.jpg',
       imageOrder: '2',
       textOrder: '1',
@@ -22,19 +26,11 @@ export class HomeComponent implements OnInit {
       subUrl: './assets/call-to-action-bg.jpg'
     },
     { title: 'Health Care',
-      description: `Gone are the days when patients were just … well, patients. In today’s era
-        of instant and digital everything, patients see themselves as consumers of
-        healthcare services, products, and experiences.
-        Patients want to clearly understand what they are receiving, how, and why.
-        In fact, 1 in 5 patients want to be told about their healthcare costs upfront,
-        while 75 percent desire to be involved in key treatment decisions.
-        Patients desire better security and more privacy. They demand that their
-        personal information and medical records are secure and safe. More importantly,
-        patients want increased control in how their health data is handled and shared.
-        So how can providers reach the demands of modern day patients? It’s simple
-        - with robust healthcare analytics.
-        With better healthcare analytics, hospitals and healthcare providers can treat
-        patients better and offer the much-needed holistic patient experiences.`,
+      description: `In fact, 1 in 5 patients want to be told about their healthcare costs upfront, while 75 percent desire to be
+      involved in key treatment decisions. More importantly, patients want increased control in how their health data is handled
+      and shared. So how can providers reach the demands of modern day patients? It’s simple - with robust healthcare analytics.
+      With better healthcare analytics, hospitals and healthcare providers can treat patients better and offer the much-needed holistic
+      patient experiences.`,
       mainUrl: './assets/healthcare.jpg',
       imageOrder: '1',
       textOrder: '2',
@@ -49,22 +45,13 @@ export class HomeComponent implements OnInit {
       subUrl: './assets/healthcare-bg.jpg'
     },
     { title: 'Manufacturing',
-      description: `Most manufacturers have already made the most obvious changes to streamline their operations,
-      using traditional methods to eke as much productivity out of their supply chains and plants as possible.
-      To do even more with less in a slow-growth and uncertain environment, however, companies must look for 
-      new ways to boost the productivity and profitability of their operations.
-      There’s one significant asset that manufacturers have not yet optimized: their own data.
-      Process industries generate enormous volumes of data, but many have failed to make use of this
-      mountain of potential intelligence. Historically, manufacturers have lagged other industries in their IT capabilities.
-      However, thanks to cheaper computational power and rapidly advancing analytics opportunities,
-      process manufacturers can put that data to work, gathering information from multiple data sources and
-      taking advantage of machine learning models and visualization platforms to uncover new ways to optimize
-      their processes from the sourcing of raw materials to the sale of their finished products.
-      Advanced analytics also help manufacturers solve previously impenetrable problems and reveal those that
-      they never knew about, such as hidden bottlenecks or unprofitable production lines.
-      here are three applications of advanced analytics in particular that together are
-      powerful tools for maximizing the physical and financial performance of manufacturers’ assets and
-      often-complex supply chains.`,
+      description: `There’s one significant asset that manufacturers have not yet optimized: their own data. Process industries generate
+      enormous volumes of data, but many have failed to make use of this mountain of potential intelligence. Historically, manufacturers
+      have lagged other industries in their IT capabilities. However, thanks to cheaper computational power and rapidly advancing analytics
+      opportunities, process manufacturers can put that data to work, gathering information from multiple data sources and taking advantage
+      of machine learning models and visualization platforms to uncover new ways to optimize their processes from the sourcing of raw materials
+      to the sale of their finished products. Advanced analytics also help manufacturers solve previously impenetrable problems and reveal those
+      that they never knew about, such as hidden bottlenecks or unprofitable production lines.`,
       mainUrl: './assets/manufacturing.jpg',
       imageOrder: '2',
       textOrder: '1',
@@ -79,9 +66,15 @@ export class HomeComponent implements OnInit {
       subUrl: './assets/manufacturing-bg.png'
     }
   ]
+  innerWidth: number;
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event']) onResize(event) {
+    this.innerWidth = window.innerWidth;
   }
 
 }
